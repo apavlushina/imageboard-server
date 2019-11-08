db6f17d7a2bb65c23141588acc77d394af8c66b4
apavlushina <ssemina7@gmail.com>
2019-11-08 09:38:55 +0100

    start server

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+app.get("/test", (req, res) => res.send("Hello stranger!"));
+app.listen(port, () => console.log(`Listening on port ${port}`));
