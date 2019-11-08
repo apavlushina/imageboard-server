9bc47fc1f421c82eef2303fb4da8a8a0ec2595cb
apavlushina <ssemina7@gmail.com>
2019-11-08 10:17:54 +0100

    db connected

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const app = express();
 const port = process.env.PORT || 4000;
 app.get("/test", (req, res) => res.send("Hello stranger!"));
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+const db = require("./db");
